21f4ec5b9fb9f37f7f48dc7a96e905b2f70f28ca
Zhou Xinzi <imdreamrunner@gmail.com>
2015-10-27 12:10:50 +0800

    add port config

--- debug.py
+++ debug.py
@@ -1,10 +1,15 @@
 __author__ = "Xinzi Zhou"
 __email__ = "imdreamrunner@gmail.com"
 
+
+import sys
 import logging
 from app import app
 
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    port = 5000
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    app.run(host='0.0.0.0', debug=True, port=port)
